bbee5c39ebc7665317765cda0dd9eac2eced549e
Devesh <deveshdev2909@gmail.com>
2023-09-03 10:33:19 +0530

    used f strings

--- multiplication-table.py
+++ multiplication-table.py
@@ -1,4 +1,5 @@
 num=int(input('Enter a number : '))
 for i in range(1,11):
-    print(str(num),'x',str(i),'=',num*i)
+    # print(str(num),'x',str(i),'=',num*i)
+    print(f"{num}x{i}={num*i}")    # f strings
     i=i+1
