dc71bc17c38e7f4752d7446a543142ae6d23026e
Devesh <deveshdev2909@gmail.com>
2023-09-03 10:29:32 +0530

    loops in python

---
+++ for-loop.py
@@ -0,0 +1,8 @@
+# printing content of a list with for loop
+fruit=['banana','apple','mango','watermelon','grapes']
+for  i in fruit:
+    print(i)
+
+# printing 1-11 with for loop 
+for a in range(1,11):
+    print(a)
---
+++ while-loop.py
@@ -0,0 +1,12 @@
+# printing 1-50 with while loop 
+i=1
+while i<=50:
+    print(i)
+    i = i+1
+
+# printing content of a list with while Loop 
+fruit=['banana','apple','mango','watermelon','grapes']
+i=0
+while i<len(fruit):
+    print(fruit[i])
+    i=i+1
